1f8e89bca03d156df78f0f65402e42e62e381c0d
aongtosan <aongtosan@gmail.com>
2021-10-24 10:12:38 +0700

    update pokedex structure

--- pokemon-api-test/src/App/App.js
+++ pokemon-api-test/src/App/App.js
@@ -5,7 +5,18 @@ function App() {
   const [pokemon,setPokemon] = useState([])
   return (
     <div className="App">
-      <PokemonList/>
+            <div className="Container">
+              <div id="title">Pokedex</div>
+              <div id="main">
+              <div> Main Area : default search </div>
+              <PokemonList/>
+              </div>
+              <div id="sidebar">Sidebar</div>
+              <div id="content1">Container1 </div>
+              <div id="content2">Container2 </div>
+              <div id="content3">Container3 </div>
+            <div id="footer">Footer</div>
+          </div>
     </div>
   );
 }
